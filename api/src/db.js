faa76a0d50f56be93de28446097629252cab63f7
Daniel Zapata <daniel.zapata.grajales@gmail.com>
2021-11-09 17:59:50 -0500

    db

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const {Sequelize, DataTypes} = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const {DB_USER,DB_NAME, DB_HOST,DB_PORT} = process.env;
+const {DB_USER,DB_NAME, DB_HOST,DB_PORT, DB_PASSWORD} = process.env;
 
 
 /*  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, */  /* Henry option */
