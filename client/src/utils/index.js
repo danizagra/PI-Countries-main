057e92e464f065d3b56d09ebb2910f8217b2918d
Daniel Zapata <daniel.zapata.grajales@gmail.com>
2021-12-09 14:18:09 -0500

    vercel changes

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "";
 export const COUNTRY_URL = BASE_URL + "/countries";
 export const ACTIVITY_URL = BASE_URL + "/activity";
 export const SEARCH_URL = COUNTRY_URL + '?name='
